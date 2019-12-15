87ddee351bee6d743a2a2d8f17ce630880041a08
Oleg Titov <oleggtitoff@gmail.com>
2019-12-15 21:20:26 +0200

    Added empty class Vegetable with annotations.

---
+++ src/main/java/ua/training/model/entity/Vegetable.java
@@ -0,0 +1,13 @@
+package ua.training.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Vegetable {
+
+}
