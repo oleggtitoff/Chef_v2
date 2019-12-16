43628dc7eafb53734fbccddc2c9574d9f0e64909
Oleg Titov <oleggtitoff@gmail.com>
2019-12-16 02:42:40 +0200

    Added Controller class (all tests are green).

---
+++ src/main/java/ua/training/controller/Controller.java
@@ -0,0 +1,10 @@
+package ua.training.controller;
+
+import ua.training.model.entity.Salad;
+import ua.training.view.View;
+
+public class Controller {
+    private Salad salad;
+    private View view;
+
+}
