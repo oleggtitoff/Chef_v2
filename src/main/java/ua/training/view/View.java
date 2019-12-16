2c90eca5f8e48afccf2021e382a182e4d229bb5d
Oleg Titov <oleggtitoff@gmail.com>
2019-12-16 02:20:45 +0200

    Added resources: en and uk .properties files. In View added resource bundle (all tests are green).

--- src/main/java/ua/training/view/View.java
+++ src/main/java/ua/training/view/View.java
@@ -1,5 +1,14 @@
 package ua.training.view;
 
-public class View {
+import java.util.Locale;
+import java.util.ResourceBundle;
 
+public class View {
+    private static final String MESSAGES_BUNDLE_NAME = "messages";
+    // English
+    private static final Locale locale = new Locale("en", "US");
+    // Ukrainian
+    //private static final Locale locale = new Locale("uk", "UA");
+    public static final ResourceBundle bundle = ResourceBundle.getBundle(
+            MESSAGES_BUNDLE_NAME, locale);
 }
