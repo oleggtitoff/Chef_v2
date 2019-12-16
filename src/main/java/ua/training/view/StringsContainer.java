cf568682d81f243b64ece142bfece270bfb71bf9
Oleg Titov <oleggtitoff@gmail.com>
2019-12-16 02:11:24 +0200

    In StringsContainer added constants (all tests are green).

--- src/main/java/ua/training/view/StringsContainer.java
+++ src/main/java/ua/training/view/StringsContainer.java
@@ -1,5 +1,17 @@
 package ua.training.view;
 
 public interface StringsContainer {
+    String VEGETABLE_WORD = "output.vegetable.word";
+    String ADDITIONAL_INGREDIENT_WORD = "output.additional.ingredient.word";
+    String NAME_WORD = "output.name.word";
+    String MASS_WORD = "output.mass.word";
+    String CALORIES_WORD = "output.calories.word";
+    String CALORIES_IN_1_G_WORD = "output.calories.in1kg.word";
+    String SALAD_WORD = "output.salad.word";
 
+    String INGREDIENT_ADDED_MESSAGE = "input.ingredient.added.message";
+    String SORT_MESSAGE = "output.sort.message";
+    String RANGE_MESSAGE = "output.range.message";
+    String TOTAL_CALORIES_MESSAGE = "output.total.calories.message";
+    String INGREDIENTS_MESSAGE = "output.ingredients.message";
 }
